7faa6088a7dcb5d2d808a400be6cf1cf0bf92886
Ain Mutaqorrobin <robin@mesiniaga.com.my>
2024-02-19 16:37:21 +0800

    adjust functional navigation bar using event admitter (will improve soon)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: `app-header`,
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   collapsed: boolean = true;
+  @Output() optionChoosed = new EventEmitter<string>();
+
+  onClick(option: string) {
+    this.optionChoosed.emit(option);
+  }
 }
