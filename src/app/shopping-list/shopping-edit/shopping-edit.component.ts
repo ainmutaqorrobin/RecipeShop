43d1ebd67f67dd223e0474e059a4c487316c6006
Ain Mutaqorrobin <robin@mesiniaga.com.my>
2024-02-20 14:15:12 +0800

    code 2:15 P.M

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../../recipes/recipe.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrl: './shopping-edit.component.css'
+  styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
+  nameInput: string;
+  amountInput: string;
 
+  addNewItem(
+    localNameInput: HTMLInputElement,
+    localAmountInput: HTMLInputElement
+  ) {
+    this.nameInput = localNameInput.value;
+    this.amountInput = localAmountInput.value;
+
+    console.log(
+      `this.nameInput is ` +
+        this.nameInput +
+        ` and this.amountInput is ` +
+        this.amountInput
+    );
+  }
 }
