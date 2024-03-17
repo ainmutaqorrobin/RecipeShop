b2e2ac76a9f2a3db1764fb3031eafa50deadf2be
mutaqorrobin <robin@mesiniaga.com.my>
2024-03-17 23:08:47 +0800

    added ngForm also some regex pattern for validation

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, SimpleChange, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Ingredients } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../../services/shopping-list.service';
+import { FormControl, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,19 +9,23 @@ import { ShoppingListService } from '../../services/shopping-list.service';
   styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
-  @ViewChild(`nameInput`, { static: false }) __nameInput: ElementRef;
-  @ViewChild(`amountInput`, { static: false }) __amountInput: ElementRef;
+  // @ViewChild(`nameInput`, { static: false }) __nameInput: ElementRef;
+  // @ViewChild(`amountInput`, { static: false }) __amountInput: ElementRef;
 
   constructor(private SLService: ShoppingListService) {}
 
-  addNewItem() {
-    const ingName = this.__nameInput.nativeElement.value;
-    const ingAmount = this.__amountInput.nativeElement.value;
-    const newIngredient = new Ingredients(ingName, ingAmount);
-    this.SLService.addIngredient(newIngredient);
-  }
+  // addNewItem() {
+  //   const ingName = this.__nameInput.nativeElement.value;
+  //   const ingAmount = this.__amountInput.nativeElement.value;
+  //   const newIngredient = new Ingredients(ingName, ingAmount);
+  //   this.SLService.addIngredient(newIngredient);
+  // }
 
-  deleteItem() {
-    
+  deleteItem() {}
+
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredients(value.name, value.amount);
+    this.SLService.addIngredient(newIngredient);
   }
 }
