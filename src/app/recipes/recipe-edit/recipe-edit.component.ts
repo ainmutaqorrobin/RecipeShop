aeffa140c56eec8752c14cc999eaeea34de8e447
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-03-21 00:00:11 +0800

    added validator for recipe edit

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../../services/recipe.service';
 import { Ingredients } from '../../shared/ingredient.model';
@@ -47,21 +47,36 @@ export class RecipeEditComponent implements OnInit {
         for (let ingredient of recipe.ingredients)
           recipeingredients.push(
             new FormGroup({
-              name: new FormControl(ingredient.name),
-              amount: new FormControl(ingredient.amount),
+              iname: new FormControl(ingredient.name, Validators.required),
+              iamount: new FormControl(ingredient.amount, [
+                Validators.required,
+                Validators.pattern('^[1-9]+[0-9]*$'),
+              ]),
             })
           );
       }
     }
     this.recipeForm = new FormGroup({
-      name: new FormControl(recipeName),
-      imagePath: new FormControl(recipeImagePath),
-      description: new FormControl(recipeDescription),
+      name: new FormControl(recipeName, Validators.required),
+      imagePath: new FormControl(recipeImagePath, Validators.required),
+      description: new FormControl(recipeDescription, Validators.required),
       ingredients: recipeingredients,
     });
   }
 
   onSubmit() {
     console.log(this.recipeForm.value);
+  }
+
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        iname: new FormControl(null, Validators.required),
+        iamount: new FormControl(null, [
+          Validators.required,
+          Validators.pattern('^[1-9]+[0-9]*$'),
+        ]),
+      })
+    );
   }
 }
