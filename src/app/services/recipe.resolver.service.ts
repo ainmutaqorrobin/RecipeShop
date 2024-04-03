1de6c142d0a967e5c86a0d02a99706ac30e1e47a
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-04-03 15:33:52 +0800

    fixed some bugs where resolver will saved edited recipe locally

--- src/app/services/recipe.resolver.service.ts
+++ src/app/services/recipe.resolver.service.ts
@@ -7,14 +7,21 @@ import {
 import { Recipe } from '../recipes/recipe.model';
 import { DataStorageService } from './data-storage.service';
 import { Observable } from 'rxjs';
+import { RecipeService } from './recipe.service';
 
 @Injectable()
 export class RecipeResolverService implements Resolve<Recipe[]> {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipeService: RecipeService
+  ) {}
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
-    return this.dataStorageService.fetchRecipes();
+    const recipes = this.recipeService.getRecipes();
+
+    if (recipes.length === 0) return this.dataStorageService.fetchRecipes();
+    else return recipes;
   }
 }
