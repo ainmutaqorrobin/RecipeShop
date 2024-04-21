b3231bc76c520d81faa121137f2e3157c7d16356
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-04-21 21:13:45 +0800

    enhance UI at recipe page

--- src/app/recipes/recipe-start/recipe-start.component.ts
+++ src/app/recipes/recipe-start/recipe-start.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../../services/recipe.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-start',
   templateUrl: './recipe-start.component.html',
-  styleUrl: './recipe-start.component.css'
+  styleUrl: './recipe-start.component.css',
 })
-export class RecipeStartComponent {
-
+export class RecipeStartComponent implements OnDestroy {
+  recipes: Recipe[];
+  subscription: Subscription;
+  constructor(private recipeService: RecipeService) {
+    this.recipes = recipeService.getRecipes();
+    this.subscription = this.recipeService.recipeChanged.subscribe(
+      (recipes: Recipe[]) => {
+        this.recipes = recipes;
+      },
+      (error) => {}
+    );
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
